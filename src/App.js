5720f19ef54c7b961bcf7f4160eab52f580512ef
yevhen_rud <yevhen_rud@epam.com>
2022-07-05 16:38:54 +0300

    added two games

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
-import CreateElementMethod from './components/CreateElementMethod';
-import ComponentMethod from './components/ComponentMethod';
-import PureComponent from './components/PureComponent';
-import FunctionalComponent from './components/FunctionalComponent';
+// import CreateElementMethod from './components/CreateElementMethod';
+// import ComponentMethod from './components/ComponentMethod';
+// import PureComponent from './components/PureComponent';
+// import FunctionalComponent from './components/FunctionalComponent';
+// import Tenzies from './components/Tenzies';
+import MainQuiz from './components/quizzical/MainQuiz';
 
 function App() {
   return (
     <div className="App">
-      <CreateElementMethod />
+      {/* <CreateElementMethod />
       <ComponentMethod />
       <PureComponent />
-      <FunctionalComponent/>
+      <FunctionalComponent/> */}
+      {/* <Tenzies /> */}
+      <MainQuiz />
     </div>
   );
 }
